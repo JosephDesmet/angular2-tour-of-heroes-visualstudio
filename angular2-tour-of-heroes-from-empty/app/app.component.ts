ad2365642aba058caa23c85990fc99eee36fb3df
JosephDesmet <desmetjoseph@gmail.com>
2016-10-10 10:16:26 +0200

    Completed part 4 of ToH tutorial

--- angular2-tour-of-heroes-from-empty/app/app.component.ts
+++ angular2-tour-of-heroes-from-empty/app/app.component.ts
@@ -1,18 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr Iq' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -78,19 +66,32 @@ const HEROES: Hero[] = [
         border-radius: 4px 0 0 4px;
       }
 `
-    ]
-
+    ],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
     hero: Hero = {
         id: 1,
         name: 'Windstorm'
     };
-    heroes = HEROES;
+    heroes: Hero[] = Hero[0];
     selectedHero: Hero;
+
+    ngOnInit(): void {
+        
+        this.getHeroes();
+    }
+
+    constructor(private heroService: HeroService) {
+
+    }
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
     }
 }
