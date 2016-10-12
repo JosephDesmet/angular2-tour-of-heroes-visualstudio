1fc2aa93be9bf170fab83d7f7e1087e777ccbae3
JosephDesmet <desmetjoseph@gmail.com>
2016-10-12 14:49:00 +0200

    Completed part 6 of ToH tutorial (with app-routing-module)

--- angular2-tour-of-heroes-from-empty/app/app.module.ts
+++ angular2-tour-of-heroes-from-empty/app/app.module.ts
@@ -1,46 +1,57 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import './rxjs-extensions';
 
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
-import { DashboardComponent }  from './dashboard.component';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { RouterModule }  from '@angular/router';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
+        // Alternative: use AppRoutingModule
         RouterModule.forRoot([
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
             {
                 path: '',
                 redirectTo: '/dashboard',
                 pathMatch: 'full'
             },
             {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
                 path: 'detail/:id',
                 component: HeroDetailComponent
+            },
+            {
+                path: 'heroes',
+                component: HeroesComponent
             }
         ])
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
         HeroDetailComponent,
         HeroesComponent,
-        DashboardComponent
+        HeroSearchComponent
     ],
     providers: [
-        HeroService
+        HeroService,
     ],
     bootstrap: [AppComponent]
 })
